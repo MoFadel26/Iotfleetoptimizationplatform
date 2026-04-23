2cd0c9e87031e2d970897527aa52626d6560a825
Mohammed Fadel <101219769+MoFadel26@users.noreply.github.com>
2026-04-23 22:58:54 +0300

    Refactor Vite config to include API proxiesUd

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,35 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import fs from 'fs'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
-  server: {},
-  proxy: {
-    '/optimizer': {
-      target: 'http://localhost:5001',
-      changeOrigin: true,
-      rewrite: (p) => p.replace(/^\/optimizer/, ''),
-    },
-    '/api': {
-      target: 'http://localhost:5001',
-      changeOrigin: true,
-    },
-    '/iot-device': {
-      target: 'http://172.20.10.6',
-      changeOrigin: true,
-      rewrite: (p) => p.replace(/^\/iot-device/, ''),
+  server: {
+    proxy: {
+      '/optimizer': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/optimizer/, ''),
+      },
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
+      '/iot-device': {
+        target: 'http://172.20.10.6',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/iot-device/, ''),
+      },
     },
   },
-},
   resolve: {
-  alias: {
-    // Alias @ to the src directory
-    '@': path.resolve(__dirname, './src'),
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-},
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
